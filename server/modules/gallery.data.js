f62d149101868f5d2f740ba56115d2cd5b1042f4
Badra Mohamed <moha1150@umn.edu>
2022-06-09 19:33:09 -0500

    setup complete

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    {id:2, path:'images/download-1.jps', description: 'Photo of the beach because I love them so much'},
+    {id:3, path:'images/download-2.jps', description: 'Photo of the Kaaba the only place that feels like home'},
+    {id:4, path:'images/download-3.jps', description: 'Photo of heights becuase it brings me thrill'},
+
+
 ];
 
 module.exports = galleryItems;
